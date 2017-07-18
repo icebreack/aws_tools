52aff58cac2f78ad59d9f11ab3f163164b63fbb9
icebreack <rodrigofavarini@gmail.com>
2017-07-18 16:40:12 +0100

    Comments
    Comments

--- create_ec2_example.py
+++ create_ec2_example.py
@@ -2,8 +2,8 @@
 import boto3
 ec2 = boto3.resource('ec2')
 instance = ec2.create_instances(
-    ImageId='ami-1e299d7e',
+    ImageId='ami-1e299d7e', #here you change for the image that you'll use... it is easy
     MinCount=1,
     MaxCount=1,
-    InstanceType='t2.micro')
+    InstanceType='t2.micro') #here you change the instance type.... caution!
 print instance[0].id
